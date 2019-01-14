f03b929050b70a9764dd9dff54441bc18a3efe39
0xflotus <0xflotus@gmail.com>
2019-01-14 01:18:09 +0100

    refactored index.js

--- index.js
+++ index.js
@@ -5,14 +5,16 @@ module.exports = function (input, output, cb) {
     if (err && cb) return cb(err)
     var original = String(data)
     var converted = String(data)
-    original.split('\n').forEach(function (line) {
-      if (line.indexOf('![png]') === 0) {
+    original.split('\n')
+      .filter(function (line) { 
+        return line.indexOf('![png]') === 0; 
+      })
+      .forEach(function (line) {
         var file = line.slice(7, line.length - 1)
         var im = fs.readFileSync(file)
         var src = 'data:image/png;base64,' + im.toString('base64')
         var insert = '<img src="' + src + '" />'
         converted = converted.replace(line, insert)
-      }
     })
     if (output) fs.writeFile(output, converted, function (err, data) {
       if (err && cb) return cb(err)
